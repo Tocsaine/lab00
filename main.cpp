388170b2d8fca2dfe181834cf6ffcaed4f95b5a6
Ivan Kazantsev <58134954+Tocsaine@users.noreply.github.com>
2022-09-26 10:44:43 +0300

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -8,10 +8,11 @@ using namespace std;
 int main()
 {
     int a1, a2, a3, u;
+    int seconds_in_circle=1296000;
     double r;
     std::cin >> a1 >> a2 >> a3;
     u = 3600*a1 + 60*a2 + a3;
-    r=(u*2*M_PI)/1296000;
+    r=(u*2*M_PI)/seconds_in_circle;
     std::cout << r << endl;
     return 0;
 
